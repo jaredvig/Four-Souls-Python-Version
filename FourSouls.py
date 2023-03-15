20d589c71c2fd8ca8d896f304aa0e2e3086f70b8
Jared Vigliecca <jaredvig@gmail.com>
2023-03-14 23:57:35 -0700

    started creating the necessary json files for storing cards, and turn structure

--- FourSouls.py
+++ FourSouls.py
@@ -2,13 +2,29 @@ __author__ = "Jared Vigliecca"
 __date__ = "3/14/2023"
 import sys
 import random
-
+import json
 
 ######################################################################################################
 # CONSTANTS
 ######################################################################################################
 DEFAULT_AI_FILE = "foursoulsplayer"
 DEFAULT_AI_LEVEL = 4
+DEFAULT_STARTING_COINS = 3
+DEFAULT_STARTING_LOOT = 3
+DEFAULT_STARTING_ITEMS = 0
+DEFAULT_STARTING_SHOP_SIZE = 2
+DEFAULT_STARTING_ACTIVE_MONSTERS = 2
+DEFAULT_LOOT_DRAW_PER_TURN = 1
+DEFAULT_LOOT_PLAY_PER_TURN = 1
+DEFAULT_BUYABLE_SHOP_ITEMS_PER_TURN = 1
+DEFAULT_FIGHTABLE_MONSTERS_PER_TURN = 1
+DEFAULT_SOULS_NEEDED_TO_WIN = 4
+DEFAULT_CHARACTER_CARDS = "characters.json"
+DEFAULT_LOOT_CARDS = "loot_cards.json"
+DEFAULT_ITEM_CARDS = "item_cards.json"
+DEFAULT_MONSTER_CARDS = "monsters.json"
+DEFAULT_BONUS_SOULS = "bonus_souls.json"
+DEFAULT_STARTING_ITEMS = "starting_items_dict.json"
 ###############################################################################
 # Human Player Class
 ##############################################################################
@@ -59,11 +75,19 @@ class LootCard:
 #Class for the items that keeps track of their effects, costs, and other pertinent information
 #######################################################
 class Item:
+    def __init__(name, description, cost, use_code):
+        print("Make Item")
 
 #######################################################
 #Class for the monsters that keeps track of their health, damage, and other pertinent stats
 #######################################################
 class Monster:
+    def __init__(name,description,health,dice,attack,effects,reward,souls):
+        print("Make monster")
+    def __init__(name,description,type,effects):
+        print("Make Event")
+    def __init__(name,description,type,effects):
+        print("Make Event")
 
 #######################################################
 #Class for the whole player character that will be used in the game
@@ -72,28 +96,27 @@ class Player_Character:
     #class variables
 
     #character functions
-    def __init__(self,character_card,coins = 3,num_loot_cards=3):
-        self.coins = coins
+    def __init__(self,character_card, player):
+        self.coins = 0
+        self.player= player
         character_card.charged = False
         self.character_card = character_card
-        self.num_loot_cards = num_loot_cards
+        self.num_loot_cards = 0
         self.loot_cards = []
         self.items = []
         self.can_attack = False
         self.free_loot_card = False
         self.can_buy_item = False
         self.souls = 0
-        for i in range(num_loot_cards):
-            self.draw_loot_card()
         for item in self.__items:
             item.charged = False
     def draw_loot_card(self,loot_deck):
         print("draw loot card")
         self.loot_cards.append(loot_deck.remove(0))
-
-    def start_turn(self):
-         self.draw_loot_card()
-         for item in self.__items:
+        self.num_loot_cards +=1
+    def start_turn(self,loot_deck):
+         self.draw_loot_card(loot_deck)
+         for item in self.items:
             item.charged = True
          self.character_card.charged = True
     def add_coins(self,coins):
@@ -102,6 +125,9 @@ class Player_Character:
         self.can_attack = False
         self.free_loot_card = False
         self.can_buy_item = False
+    def discard_loot_card(self,loot_card,discarded_loot):
+        self.loot_cards.remove(loot_card)
+        self.num_loot_cards -=1
 
 
     
@@ -149,33 +175,104 @@ def parse_command_line_args(args):
     total_num_players = -1
     if "-p" in args:
         total_num_players = int(args[args.index("-p") + 1])
-        if total_num_players < 2 or total_num_players>4: print("please choose 2-4 players"), exit
+        if total_num_players < 2 or total_num_players>4: print("please choose 2-4 players"), sys.exit(1)
     else: players = []*total_num_players
     num_ai = -1
-    if "-a" in args:
+    if "-ai" in args:
         num_ai = int(args[args.index("-a") + 1])
-        if num_ai > total_num_players: print("Please have less ais, than total players"), exit 
+        if num_ai > total_num_players: print("Please have less ais, than total players"), sys.exit(1) 
         else:
             for i in range(total_num_players-num_ai):
                 players.append(None)
             for i in range(num_ai):
                 players.append(ai_file)
     
-    if "-l" in args:
+    if "-mml" in args:
         levels = args[args.index("-l") + 1].split(',')
         if len(levels) == 1: levels = (int(levels[0]), int(levels[0]), int(levels[0]), int(levels[0]))
         else: levels = (int(levels[0]), int(levels[1]), int(levels[2]), int(levels[3]))
     else: levels = (DEFAULT_AI_LEVEL, DEFAULT_AI_LEVEL)
     return(players, levels)
 
-def choose_random_player_character():
-    print("Random player")
-
+def shuffle(deck):
+    random.shuffle(deck)
 
 def play_game(players):
     """
     Initiation and main loop for the game
     """
+    """
+    Initializing all the pertinent decks required.
+    """
+    monster_deck_file = open(DEFAULT_MONSTER_CARDS)
+    monster_deck = json.load(monster_deck_file)
+    loot_deck = json.loads(DEFAULT_LOOT_CARDS)
+    treasure_deck = json.loads(DEFAULT_ITEM_CARDS)
+    characters = json.loads(DEFAULT_CHARACTER_CARDS)
+    starting_items = json.loads(DEFAULT_STARTING_ITEMS)
+    discarded_loot_deck = []
+    discarded_item_deck = []
+    discarded_monster_deck = []
+    shop = []
+    active_monsters = []
+    #Shuffle all of the decks that need to be randomly drawn from
+    shuffle(monster_deck)
+    shuffle(loot_deck)
+    shuffle(treasure_deck)
+    shuffle(characters)
+
+    #give all of our players a random character, and give them the default number of coins and loot cards
+    player_characters = []
+    for player in players:
+        player_character = Player_Character(characters.remove(random.randrange(len(characters))),player)
+        player_character.add_coins(DEFAULT_STARTING_COINS)
+        for i in range(DEFAULT_STARTING_LOOT):
+            player_character.draw_loot_card(loot_deck)
+        player_characters.append(player_character)
+    player_has_won = False
+    
+    #CHOOSE PLAYER ORDER HERE!!!!!!!!!!!!!!
+    #
+    #
+    #
+    #
+    starting_player = 0 #PLACEHOLDER UNTIL A BETTER WAY TO FIND THE STARTING PLAYER IS FOUND
+    current_player = starting_player
+
+
+    for i in range(DEFAULT_STARTING_SHOP_SIZE):
+        treasure = treasure_deck.remove(random.randrange(len(treasure_deck)))
+        shop.append(treasure)
+    for i in range(DEFAULT_STARTING_ACTIVE_MONSTERS):
+        active_monster = monster_deck.remove(random.randrange(len(monster_deck)))
+        while active_monster.type != "Monster":
+            monster_deck.append(active_monster)
+            active_monster = monster_deck.remove(random.randrange(len(monster_deck)))
+        active_monsters.append(active_monster)
+
+
+        #Our main loop, goes until a player has won or a tie has happened.
+    while(not player_has_won):
+
+        current_player_character = player_characters[current_player]
+        current_player_character.start_turn(loot_deck)
+        #######################################
+        #
+        #WRITE WHAT HAPPENS IN A TURN HERE
+        #
+        #######################################
+        current_player_character.end_turn()
+        current_player = (current_player+1)%len(players)
+
+
+        
+
+    
+
+        
+        
+
+
 
 
 ######################################################################################################
@@ -192,7 +289,7 @@ if __name__ == "__main__":
         players.append(load_player(i,player_file,levels[i]))
         i+=1
 
-    play
+    play_game(player_files)
 
 
 
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 A simpler version of the card game "Binding of Isaac: Four Souls", built in python, which can use user created ais, as well as supports 2-4 player, currently only on one machine.
 
 Made with the intention of creating an AI that can then play the game.
+
+If you want to create custom cards, I would recommend looking at the examples and templates that I have provided,
+as they give a basic idea of how to create each card, and allows for these to be more modular without having to edit the source code.
+Additionally, this is a very basic version of four souls, that among other things does not have online or graphics, so it probably is not a suitable replacement for the real version.
+I would recommend purchasing the game, or playing the tabletop simulator version instead of this, as it is a pretty rough experience.
