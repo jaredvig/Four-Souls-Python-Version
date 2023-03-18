aad883faa35fe193f4cd8a59a6fe773062ef24e7
Jared Vigliecca <jaredvig@gmail.com>
2023-03-18 16:55:34 -0700

    added a txt file for general effects and targetting, and but the rest is a hot fucking mess

--- FourSouls.py
+++ FourSouls.py
@@ -9,6 +9,7 @@ import json
 ######################################################################################################
 DEFAULT_AI_FILE = "foursoulsplayer"
 DEFAULT_AI_LEVEL = 4
+DEFAULT_SHOP_PRICE = 10
 DEFAULT_STARTING_COINS = 3
 DEFAULT_STARTING_LOOT = 3
 DEFAULT_STARTING_ITEMS = 0
@@ -61,12 +62,10 @@ class Character:
 #Class for the loot cards that keeps track of their effects
 #######################################################
 class LootCard:
-    def __init__(self,name,description,code_string):
+    def __init__(self,name,description,effect):
         self.name = name
         self.description = description
-        self.code_string = code_string
-    def activate_card_effects(self):
-        exec(self.code_string)
+        self.effect = effect
     def to_string(self):
         return self.name,self.description
 
@@ -98,36 +97,44 @@ class Player_Character:
     #character functions
     def __init__(self,character_card, player):
         self.coins = 0
-        self.player= player
+        self.player_type= player
         character_card.charged = False
         self.character_card = character_card
-        self.num_loot_cards = 0
         self.loot_cards = []
         self.items = []
-        self.can_attack = False
+        self.curses = []
+        self.available_attacks = 1
         self.free_loot_card = False
-        self.can_buy_item = False
+        self.buyable_items = 1
         self.souls = 0
         for item in self.__items:
             item.charged = False
-    def draw_loot_card(self,loot_deck):
+    def draw_loot_card(self,loot_card):
         print("draw loot card")
-        self.loot_cards.append(loot_deck.remove(0))
-        self.num_loot_cards +=1
-    def start_turn(self,loot_deck):
-         self.draw_loot_card(loot_deck)
-         for item in self.items:
-            item.charged = True
-         self.character_card.charged = True
+        self.loot_cards.append(loot_card)
+
+
     def add_coins(self,coins):
         self.coins+=coins
+
+    def subtract_coins(self,coins_to_subtract):
+        self.coins-=coins_to_subtract
+
+    def add_curse(self, curse):
+        self.curses.append(curse)
+    def add_item(self,item):
+        self.items.append(item)
+
+    def lose_item(self,item):
+        self.items.remove(item)
+
     def end_turn(self):
         self.can_attack = False
         self.free_loot_card = False
         self.can_buy_item = False
+
     def discard_loot_card(self,loot_card,discarded_loot):
         self.loot_cards.remove(loot_card)
-        self.num_loot_cards -=1
 
 
     
@@ -197,7 +204,10 @@ def parse_command_line_args(args):
 def shuffle(deck):
     random.shuffle(deck)
 
+
+
 def play_game(players):
+    ###DEFINING GAMEWIDE FUNCTIONS###
     """
     Initiation and main loop for the game
     """
@@ -214,6 +224,7 @@ def play_game(players):
     discarded_item_deck = []
     discarded_monster_deck = []
     shop = []
+    shop_price = DEFAULT_SHOP_PRICE
     active_monsters = []
     #Shuffle all of the decks that need to be randomly drawn from
     shuffle(monster_deck)
@@ -237,32 +248,178 @@ def play_game(players):
     #
     #
     starting_player = 0 #PLACEHOLDER UNTIL A BETTER WAY TO FIND THE STARTING PLAYER IS FOUND
-    current_player = starting_player
-
+    current_active_player = starting_player
+    stack = []
 
     for i in range(DEFAULT_STARTING_SHOP_SIZE):
         treasure = treasure_deck.remove(random.randrange(len(treasure_deck)))
         shop.append(treasure)
     for i in range(DEFAULT_STARTING_ACTIVE_MONSTERS):
-        active_monster = monster_deck.remove(random.randrange(len(monster_deck)))
-        while active_monster.type != "Monster":
-            monster_deck.append(active_monster)
+        initial_monster = monster_deck.remove(random.randrange(len(monster_deck)))
+        while initial_monster.type != "Monster":
+            monster_deck[i].append(active_monster)
             active_monster = monster_deck.remove(random.randrange(len(monster_deck)))
         active_monsters.append(active_monster)
-
-
+    shuffle(monster_deck)
+    
         #Our main loop, goes until a player has won or a tie has happened.
     while(not player_has_won):
+        #ALL OF THE WORK BEFORE THE PLAYER CAN DO THINGS ON THEIR TURN
+        current_turn_finished = False
+        active_player_character = player_characters[current_active_player]
+        start_turn(active_player_character)
+        #############################################################
+        #Active player choice
+        while not current_turn_finished:
+            active_player_choice = get_active_player_choice(active_player_character)
+            #PLAY FREE LOOT CARD, or activate player card
+            if active_player_choice == 1:
+                loot_card, target = get_loot_card_choice(active_player_character)
+                play_loot_card(loot_card,target)
+                #PLAY ITEM
+            elif active_player_choice == 2:
+                item, target = get_item_choice(active_player_character)
+                use_item(item,target)
+                #ATTACKING MONSTER DECK
+            elif active_player_choice == 3:
+                attacked_slot = get_attacked_monster(active_player_choice)
+                active_monster
+                if attacked_slot == 0:
+                    covered_slot = get_monster_slot(active_player)
+                    uncover_monster(covered_slot)
+                    new_monster = monster_deck.remove(0)
+                    active_monsters[covered_slot].append(monster_deck.remove(0))
 
-        current_player_character = player_characters[current_player]
-        current_player_character.start_turn(loot_deck)
+                else:
+                    active_monster = active_monsters[attacked_slot].pop()
+                
+                if active_monster.type == "Monster":
+                    print("Monster")
+                elif active_monster.type == "Curse":
+                    print("Curse")
+                    curse = active_monster
+                    target = choose_player(active_player_character)
+                    target.add_curse(curse)
+
+                
+                else: #Card is an event card
+                    print("Event")
+                    
+                
+
+                    
+
+            #Buy Item
+            elif active_player_choice == 4:
+                print("buy item")
+
+            #End your turn
+            elif active_player_choice == 5:
+                print("end turn")
+                current_turn_finished = True
+            #Invalid choice 
+            else: print("Sorry, the inputted value is invalid, please try again")
         #######################################
-        #
-        #WRITE WHAT HAPPENS IN A TURN HERE
-        #
-        #######################################
-        current_player_character.end_turn()
-        current_player = (current_player+1)%len(players)
+        active_player_character.end_turn()
+        current_active_player = (current_active_player+1)%len(players)
+
+
+        #####Defining game functions
+    def start_turn(active_player):
+        for item in active_player.items:
+            item.charged = True
+        active_player.character_card.charged = True
+        active_player.can_attack = True
+        active_player.free_loot_card = True
+        active_player.can_buy_item = True
+        active_player.draw_loot_card(loot_deck(0))
+   
+    def end_turn(active_player):
+        active_player.can_attack = False
+        active_player.free_loot_card = False
+        active_player.can_buy_item = False
+
+    def purchase_item(active_player, shop_index):
+        if(shop_index == 0):
+            active_player.items.append(treasure_deck.remove[0])
+        else:
+            active_player.items.append(shop.remove[shop_index-1])
+            shop.append(treasure_deck.remove[0])
+        active_player.remove_coins(shop_price)
+
+    def discard_card(card,card_type):
+        if(card_type == "monster"):
+            discarded_monster_deck.append(card)
+        elif(card_type == "item"):
+            discarded_item_deck.append(card)
+        elif(card_type == "loot"):
+            discarded_item_deck.append(card)
+    def cancel_next_effect():
+        stack.pop()
+    def get_active_player_choice(player):
+        if player.player_type is None: 
+            print("What are you going to do this turn?")
+            print("Choose 1 for playing a loot card")
+            print("Choose 2 for activating an item")
+            print("Choose 3 for fighting a monster")
+            print("Choose 4 for buying an item")
+            print("Choose 5 to end your turn")
+            return input()
+   
+    def priority(player):
+        for i in range(len(players)-1):
+            player_priority = (player +i+1)%len(players)
+            #######PROMPT PLAYER FOR MOVE########
+            #######SKIP MEANS RETURN None########
+            #######USE CARD##############
+            used_card = get_player_input()
+            effect = None
+            if(used_card == None):
+                continue
+            else:
+                if(type(used_card) is LootCard):
+                    effect = play_loot_card(used_card)
+                elif(type(used_card) is Item):
+                    effect = play_item(used_card)
+                else:
+                    effect = use_character(used_card)
+                stack.append(effect)
+                priority(player_priority)
+        exec(stack.pop())
+    def choose_loot_card(player):
+        loot_cards = player.loot_cards
+        loot_index = 1
+        print("Please choose a loot card:")
+        if player.player_type is None:
+            for loot_card in loot_cards:
+                print("Type ", loot_index, " to use ", loot_card.name)
+            return input()
+    def play_loot_card(player):
+        loot_index = choose_loot_card(player)
+        loot_card = player.loot_cards[loot_index-1]
+        effects, targets = get_effects_and_targets(loot_card)
+        total_effect_string = ""
+        for effect in effects:
+            for target in targets:
+                effect_string = effect,"(",target,");"
+                total_effect_string+=effect_string
+        return total_effect_string
+        
+        #USE ALL APPLICABLE LISTS FOR A TARGET
+    def choose_target(target_list):
+        if player.player_type is None:
+            target_index = 1
+            for target in target_list:
+                print("Type ", target_index, " to target: ", target.name)
+            return target_list[input()]
+        
+
+        
+
+
+
+
+
 
 
         
